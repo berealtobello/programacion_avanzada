1e055e72dccaa5e5d2d0452a46358c792a649340
berealtobello <berenice.altobello@edu.uah.es>
2020-02-21 16:57:35 +0100

    clase 21 de febrero

--- sesion4.R
+++ sesion4.R
@@ -1,2 +1,10 @@
 2+2
 "hola mundo soy bere"
+install.packages("rmarkdown")
+library(rmarkdown)
+if(!require(installr)) {
+  install.packages("installr"); require(installr)} #load / install+load installr
+
+# using the package:
+updateR()
+
